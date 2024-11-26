f28ba46eb039d546e5b67d576a34adab5638d334
thepowerKey888 <thepower674@gmail.com>
2024-11-26 16:35:51 -0600

    Began with user story 1 by creating a single instance of the logger and a class called logger.

--- EventLoggerPackage/Logger.java
+++ EventLoggerPackage/Logger.java
@@ -1,4 +1,26 @@
 package EventLoggerPackage;
 
 public class Logger {
+
+    //user story 1
+
+    private static Logger instance;
+    private Logger() {
+
+    }
+
+    //get the single instance of the Logger
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    //log messages (consistent logging)
+    public void log(String message) {
+        System.out.println("[LOG] " + message);
+    }
+
+
 }
---
+++ EventLoggerPackage/Main.java
@@ -0,0 +1,12 @@
+package EventLoggerPackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //user story 1
+        Logger logger = Logger.getInstance();
+
+
+    }
+}
